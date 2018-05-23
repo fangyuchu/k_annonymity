35db4157f3fc00349ad4c96f6b3c292a73d363ff
fangyuchu <569160112@qq.com>
2018-05-23 21:16:08 +0800

    5.23，加入检查文件是否为.xls

--- src/importFile.java
+++ src/importFile.java
@@ -22,7 +22,7 @@ public class importFile {
         }
         return f;
     }
-    public static String[] file(String title){                      //自动获取titile文件夹中的所有文件的文件名，在这个项目中要求这个文件夹中均为excel文件
+    public static String[] file(String title){                      //自动获取titile文件夹中的所有文件的文件名，在这个项目中要求文件为.xls文件
         File t;
         if(title.contains("\\")){
             title=title.replace("\\","/");                          //替换所有的\，若用replaceAll，则因为正则表达式的关系，需要用relpaceAll("\\\\","/")
@@ -31,13 +31,15 @@ public class importFile {
             t = new File(route + title);
         }
         File []files = t.listFiles();
-        //String[] f=new String[files.length];
         ArrayList<String> f=new ArrayList();
         for(int i=0;i<files.length;i++){
-            if(!files[i].isDirectory()) {
+            if(!files[i].isDirectory()&&checkxls(files[i].getAbsolutePath())) {
                 f.add(files[i].getAbsolutePath());
             }
         }
         return f.toArray(new String[0]);
+    }
+    public static boolean checkxls(String s){                              //检查文件是否是.xls文件
+        return s.substring(s.length()-3,s.length()-1).equals(".xls");
     }
 }
